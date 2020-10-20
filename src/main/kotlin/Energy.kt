603db02dc03d5223cdc07842ef50ca2dce2d5eba
Stefan Röpstorff <stefan.roepstorff@uxma.com>
2020-10-20 19:05:32 +0200

    add code from first session

---
+++ src/main/kotlin/Energy.kt
@@ -0,0 +1,46 @@
+
+fun berechneTaeglichenVerbrauch(verbrauch: Double, stundenProTag: Double): Double {
+    return verbrauch * stundenProTag
+}
+
+fun berechneJaehrlichenVerbrauch(verbrauch: Double, stundenProTag: Double): Double {
+    return 365 * berechneTaeglichenVerbrauch(verbrauch, stundenProTag)
+}
+
+fun berechneJaehrlichenVerbrauchKWH(verbrauch: Double, stundenProTag: Double): Double {
+    return berechneJaehrlichenVerbrauch(verbrauch, stundenProTag) / 1000.0
+}
+
+val preisProKWH = 0.3
+fun berechnePreisProJahr(verbrauch: Double, stundenProTag: Double): Double {
+    return berechneJaehrlichenVerbrauchKWH(verbrauch, stundenProTag) * preisProKWH
+}
+
+fun berechnePreis(gesamtVerbrauchKWH: Double): Double {
+    return gesamtVerbrauchKWH * preisProKWH
+}
+
+val co2ProKWH = 0.4
+fun berechneCO2(gesamtVerbrauchKWH: Double): Double {
+    return gesamtVerbrauchKWH * co2ProKWH
+}
+
+fun main() {
+
+    print("Gib den Verbrauch deines Elektrogeräts ein (in Watt): ")
+    val verbrauch: Double = readLine()?.toDouble() ?: 0.0
+    print("Wie lang ist das Gerät jeden Tag in Betrieb? (in Stunden): ")
+    val stundenProTag: Double = readLine()?.toDouble() ?: 0.0
+
+    print("Der jährliche Verbrauch (in KWH) ist: ")
+    val jaehrlicherVerbrauchKWH = berechneJaehrlichenVerbrauchKWH(verbrauch, stundenProTag)
+    println(jaehrlicherVerbrauchKWH)
+
+    print("Der Preis in einem Jahr beträgt: ")
+    val jaehrlicherPreis = berechnePreis(jaehrlicherVerbrauchKWH)
+    println(jaehrlicherPreis)
+
+    print("Der jährliche CO2-Verbrauch beträgt: ")
+    val jaehrlichesCO2 = berechneCO2(jaehrlicherVerbrauchKWH)
+    println(jaehrlichesCO2)
+}
--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,3 +1,25 @@
+
+fun addiere(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multipliezere(a: Int, b: Int): Int {
+    return a * b
+}
+
+
+
 fun main() {
-    println("Hello, world!")
+
+    print("Gib eine Zahl ein: ")
+    val ersteTextEingabe: String = readLine() ?: "0"
+    val ersteZahl = ersteTextEingabe.toInt()
+
+    print("Gib eine zweite Zahl ein: ")
+    val zweiteTextEingabe: String = readLine() ?: "0"
+    val zweiteZahl = zweiteTextEingabe.toInt()
+
+    print("Das Produkt dieser Zahlen ist: ")
+    val produkt = multipliezere(ersteZahl, zweiteZahl)
+    println(produkt)
 }
