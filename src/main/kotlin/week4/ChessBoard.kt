c199df9c72431a9ee7f282c727592e2ad542d643
Stefan Röpstorff <stefan.roepstorff@uxma.com>
2020-11-12 15:56:25 +0100

    fix array initialisation in ChessBoard.kt

--- src/main/kotlin/week4/ChessBoard.kt
+++ src/main/kotlin/week4/ChessBoard.kt
@@ -25,15 +25,32 @@ data class ChessBoard(
 
 fun main() {
 
-    val text = readLine() ?: ""
+    // Arrays können auf verschiedene Weisen initialisiert werden.
+    // Hier ein Beispiel für die Erstellung eines zweidimensionalen Arrays
+    // mit 8x8 Feldern, die jeweils anfangs den null-Wert enthalten
+    // (also quasi "leer" sind):
+    val emptyEightByEightArray: Array<Array<ChessPawn?>> = Array(8) {
+        Array(8) {
+            null
+        }
+    }
+    val chessBoard: ChessBoard = ChessBoard(emptyEightByEightArray)
 
-    val chessBoard: ChessBoard = ChessBoard(arrayOf())
-
+    // Den einzelnen Feldern können Werte zugewiesen werden
+    // (auch der null-Wert, der signalisiert, dass das Feld leer ist):
     chessBoard.fields[3][5] = ChessPawn(ChessPawnType.Queen, Colour.White)
     chessBoard.fields[0][3] = ChessPawn(ChessPawnType.Bishop, Colour.Black)
     chessBoard.fields[2][7] = null
 
+    // Da wir nicht wissen können, ob ein Feld eine Figur enthält oder leer ist,
+    // zwingt Kotlin uns, den Fall des fehlenden Wertes explizit zu berücksichtigen.
     val pawn: ChessPawn? = chessBoard.fields[6][1]
-    val currentColour: Colour? = pawn ?. colour
+
+    // Entweder müssen wir auf die Bestandteile so zugreifen, dass wir wiederum
+    // die Möglichkeit eines null-Werts zulassen (darum das ? hinter "Colour"):
+    val currentColour: Colour? = pawn?.colour
+
+    // ... oder wir müssen angeben, was wir im Falle des leeren Werts als
+    // "Standard" verwenden wollen (hier ChessPawnType.Bishop):
     val currentType: ChessPawnType = pawn?.type ?: ChessPawnType.Bishop
 }
